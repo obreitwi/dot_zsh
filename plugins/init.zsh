fa7f7bec23790eb0686526cedeb3fb2397fb38eb
Oliver Breitwieser <oliver.breitwieser@kip.uni-heidelberg.de>
2019-09-09 15:40:55 +0200

    Setup bat aliases and path prefixes

--- plugins/init.zsh
+++ plugins/init.zsh
@@ -3,7 +3,10 @@
 dir_plugins=${${0:A}:h}
 
 source ${dir_plugins}/zsh-autosuggestions/zsh-autosuggestions.zsh
+export ZSH_AUTOSUGGEST_USE_ASYNC=""
+
 source ${dir_plugins}/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+
 # history-substring-search needs to come AFTER syntax-highlithing
 source ${dir_plugins}/zsh-history-substring-search/zsh-history-substring-search.zsh
 
