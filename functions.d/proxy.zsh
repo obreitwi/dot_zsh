540aaa9bf22ef885e356ce9f502fd148822f21e4
Oliver Breitwieser <oliver.breitwieser@kip.uni-heidelberg.de>
2021-06-04 11:20:41 +0200

    move proxy related functions to own file

---
+++ functions.d/proxy.zsh
@@ -0,0 +1,15 @@
+setkipproxy() {
+    export no_proxy="localhost,127.0.0.1,.kip.uni-heidelberg.de"
+    export http_proxy=http://proxy2.kip.uni-heidelberg.de:8080
+    export ftp_proxy=http://proxy2.kip.uni-heidelberg.de:21
+    export https_proxy=http://proxy2.kip.uni-heidelberg.de:8080
+}
+zle -N setkipproxy
+
+setnoproxy() {
+    unset no_proxy
+    unset http_proxy
+    unset ftp_proxy
+    unset https_proxy
+}
+zle -N setnoproxy
