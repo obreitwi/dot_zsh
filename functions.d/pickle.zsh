37696852e9a954702bce76e8cbdb4264bcfb095e
Oliver Breitwieser <oliver.breitwieser@kip.uni-heidelberg.de>
2021-06-04 11:49:11 +0200

    Streamline pickle interaction functions

--- functions.d/pickle.zsh
+++ functions.d/pickle.zsh
@@ -1,5 +1,11 @@
+pkl_cat() {
+    for f in "$@"; do
+        python -c "import pickle, pprint; pprint.pprint(pickle.load(open('${f}', 'r')))"
+    done
+}
+
 # Open pickle file in python with name derived from argument
-pklopen() {
+pkl_open() {
     if [[ $# == 0 ]]; then
         ipython -ic "import pickle"
     elif [[ $# == 1 ]]; then
