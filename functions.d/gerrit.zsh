a75ee3eb642ce5cfe890dbe16c57f49c53a3918f
Oliver Breitwieser <oliver.breitwieser@kip.uni-heidelberg.de>
2021-06-04 12:00:51 +0200

    Move some more gerrit-related functions to own file

--- functions.d/gerrit.zsh
+++ functions.d/gerrit.zsh
@@ -32,44 +32,13 @@ grest() {
          "${@}"
 }
 
-# Fetch all open changesets and assign them their own branch:
-# gerrit_tree_view/{change number}/{patchset level}
-gerrit_fetch_changesets() {
-    git_dir="$(git rev-parse --show-toplevel)"
-    # remote branch
-    gerrit_remote="$(grep "^defaultremote=" "${git_dir}/.gitreview" | cut -d = -f 2)"
-    localbranchname=gerrit_tree_view
-
-    gerrit_remote_raw=$(git config --local remote.${gerrit_remote}.url)
-    elements=("${(@s:/:)gerrit_remote_raw}")
-    gerrit_project=${elements[-1]}
-    elements[-1]=()
-    gerrit_url=${(j./.)elements}
-
-    elements=("${(@s.:.)gerrit_url}")
-    gerrit_port=${elements[-1]}
-    gerrit_url=${${elements[-2]}:gs./.}
-
-    git branch -D $(git branch | grep "$localbranchname") &> /dev/null
-    ssh -p ${gerrit_port} ${gerrit_url} gerrit query "project:${gerrit_project} status:open" --current-patch-set | sed -n 's/\s*ref: //p' | while read REF
-do
-    git fetch "${gerrit_remote}" "${REF}"
-    git branch $(echo "${REF}" | sed "s#refs/changes/../#${localbranchname}/#") FETCH_HEAD
-done
+gerrit_build_this() {
+    gerrit_post_message -m "BUILD_THIS"
 }
 
-gerrit_query() {
-    local git_dir gerrit_branch gerrit_remote gerrit_host gerrit_port gerrit_project
-
-    git_dir="$(git rev-parse --show-toplevel)"
-
-    gerrit_branch="$(grep "^defaultbranch=" "${git_dir}/.gitreview" | cut -d = -f 2)"
-    gerrit_remote="$(grep "^defaultremote=" "${git_dir}/.gitreview" | cut -d = -f 2)"
-    gerrit_host="$(grep "^host=" "${git_dir}/.gitreview" | cut -d = -f 2)"
-    gerrit_port="$(grep "^port=" "${git_dir}/.gitreview" | cut -d = -f 2)"
-    gerrit_project="$(grep "^project=" "${git_dir}/.gitreview" | cut -d = -f 2)"
-
-    ssh -p ${gerrit_port} ${gerrit_host} gerrit query "${@}"
+gerrit_change_id_head()
+{
+    git log -n 1 | awk '$1 ~ /Change-Id:/ { print $2 }' | tr -d '\n'
 }
 
 # Get current change ids
@@ -172,3 +141,221 @@ gerrit_check_pre_review() {
     done
     rm "${tmpfile_change_info}"
 }
+
+gerrit_checkout() {
+    changenum="$(git review -l | grep "^\s*[0-9]" | fzf | awk '{ print $1 }')"
+
+    if [ -n "${changenum}" ]; then
+        git review -d "${changenum}"
+    fi
+}
+
+gerrit_cherrypick() {
+    changenums=( "${(@f)$(git review -l | grep "^\s*[0-9]" | fzf --multi | awk '{ print $1 }')}" )
+
+    for num in "${changenums[@]}"; do
+        if [ -n "${num}" ]; then
+            git review -x "${num}" || return 1
+        fi
+    done
+}
+
+# Fetch all open changesets and assign them their own branch:
+# gerrit_tree_view/{change number}/{patchset level}
+gerrit_fetch_changesets() {
+    git_dir="$(git rev-parse --show-toplevel)"
+    # remote branch
+    gerrit_remote="$(grep "^defaultremote=" "${git_dir}/.gitreview" | cut -d = -f 2)"
+    localbranchname=gerrit_tree_view
+
+    gerrit_remote_raw=$(git config --local remote.${gerrit_remote}.url)
+    elements=("${(@s:/:)gerrit_remote_raw}")
+    gerrit_project=${elements[-1]}
+    elements[-1]=()
+    gerrit_url=${(j./.)elements}
+
+    elements=("${(@s.:.)gerrit_url}")
+    gerrit_port=${elements[-1]}
+    gerrit_url=${${elements[-2]}:gs./.}
+
+    git branch -D $(git branch | grep "$localbranchname") &> /dev/null
+    ssh -p ${gerrit_port} ${gerrit_url} gerrit query "project:${gerrit_project} status:open" --current-patch-set | sed -n 's/\s*ref: //p' | while read REF
+do
+    git fetch "${gerrit_remote}" "${REF}"
+    git branch $(echo "${REF}" | sed "s#refs/changes/../#${localbranchname}/#") FETCH_HEAD
+done
+}
+
+gerrit_get_changes() {
+    grest "/a/changes/?q=$*"  | tail -n +2 | jq -r "map(.id) | .[]"
+}
+
+# Modify hashtags on the given change_id (-c)
+#
+# If change id is not specified, take the current head.
+#
+# Args:
+#   -c change_id
+#   [-a <hashtag to add>...]
+#   [-r <hashtag to remove>...]
+gerrit_hashtags() {
+    local change_id
+    local -a add
+    local -a remove
+    local -a args
+    local opts OPTIND OPTARG
+    change_id="head"
+    while getopts ":c:a:r:" opts; do
+        case "${opts}" in
+            c)
+                change_id="${OPTARG}"
+                ;;
+            a)  add+=("${OPTARG}")
+                ;;
+            r)  remove=+("${OPTARG}")
+                ;;
+            *)
+                echo "Invalid argument: ${opts}" >&2
+                return 1
+                ;;
+        esac
+    done
+    shift $(( OPTIND - 1 ))
+
+    if [ "${change_id}" = "head" ]; then
+        change_id="$(gerrit_change_id_head)"
+    fi
+
+    args=( POST "/a/changes/${change_id}/hashtags" )
+
+    if [ -n "${add[*]}" ] && [ -n "${remove[*]}" ]; then
+        echo "ERROR: No hashtags given." >&2
+        exit 1
+    fi
+
+    if [ -n "${add[*]}" ]; then
+        local add_inner="$(printf '"%s", ' "${add[@]}")"
+        args+=( "add:=[${add_inner%%, }]" )
+    fi
+
+    if [ -n "${remove[*]}" ]; then
+        local remove_inner="$(printf '"%s", ' "${remove[@]}")"
+        args+=( "remove:=[${remove_inner%%, }]" )
+    fi
+
+    grest "${args[@]}"
+}
+
+# Post a message (-m) to the given change_id (-c)
+#
+# If change id is not specified, take the current head.
+#
+# Args:
+#   -c change_id
+#   -l labels
+#   -m message
+gerrit_post_message() {
+    local change_id
+    local message
+    local labels
+    local -a args
+    local opts OPTIND OPTARG
+    change_id="head"
+    message=""
+    while getopts ":c:l:m:" opts; do
+        case "${opts}" in
+            c)
+                change_id="${OPTARG}"
+                ;;
+            m)  message="${OPTARG}"
+                ;;
+            l)  labels="${OPTARG}"
+                ;;
+            t)  topic="${OPTARG}"
+                ;;
+            *)
+                echo "Invalid argument: ${opts}" >&2
+                return 1
+                ;;
+        esac
+    done
+    shift $(( OPTIND - 1 ))
+
+    if [ "${change_id}" = "head" ]; then
+        change_id="$(gerrit_change_id_head)"
+    fi
+
+    args=( POST "/a/changes/${change_id}/revisions/current/review" )
+
+    if [ -n "${message}" ]; then
+        args+=( "message=${message}" )
+    fi
+
+    if [ -n "${labels}" ]; then
+        args+=( "labels:=${labels}" )
+    fi
+
+    grest "${args[@]}"
+}
+
+gerrit_query() {
+    local git_dir gerrit_branch gerrit_remote gerrit_host gerrit_port gerrit_project
+
+    git_dir="$(git rev-parse --show-toplevel)"
+
+    gerrit_branch="$(grep "^defaultbranch=" "${git_dir}/.gitreview" | cut -d = -f 2)"
+    gerrit_remote="$(grep "^defaultremote=" "${git_dir}/.gitreview" | cut -d = -f 2)"
+    gerrit_host="$(grep "^host=" "${git_dir}/.gitreview" | cut -d = -f 2)"
+    gerrit_port="$(grep "^port=" "${git_dir}/.gitreview" | cut -d = -f 2)"
+    gerrit_project="$(grep "^project=" "${git_dir}/.gitreview" | cut -d = -f 2)"
+
+    ssh -p ${gerrit_port} ${gerrit_host} gerrit query "${@}"
+}
+
+
+# Set a topic (-t) on the given change_id (-c)
+#
+# If change id is not specified, take the current head.
+#
+# Args:
+#   -c change_id
+#   -t topic
+gerrit_set_topic() {
+    local change_id
+    local topic
+    local -a args
+    local opts OPTIND OPTARG
+    change_id="head"
+    message=""
+    while getopts ":c:t:" opts; do
+        case "${opts}" in
+            c)
+                change_id="${OPTARG}"
+                ;;
+            t)  topic="${OPTARG}"
+                ;;
+            *)
+                echo "Invalid argument: ${opts}" >&2
+                return 1
+                ;;
+        esac
+    done
+    shift $(( OPTIND - 1 ))
+
+    if [ "${change_id}" = "head" ]; then
+        change_id="$(gerrit_change_id_head)"
+    fi
+
+    args=( PUT "/a/changes/${change_id}/topic" )
+
+    if [ -n "${topic}" ]; then
+        args+=( "topic=${topic}" )
+    else
+        echo "ERROR: -t not provided!" >&2
+        exit 1
+    fi
+
+    grest "${args[@]}"
+}
+
+
