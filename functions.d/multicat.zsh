dd9e2525dc62e5729c73a6fc6869b5328ba1544f
Oliver Breitwieser <oliver.breitwieser@kip.uni-heidelberg.de>
2021-06-04 11:22:19 +0200

    Move multicat to own file

---
+++ functions.d/multicat.zsh
@@ -0,0 +1,15 @@
+multicat() {
+    for fname in $*; do
+        for count in {0..$(( ${#fname} + 9 ))};do
+            echo -n "#"
+        done
+        echo ""
+        echo "# FILE: $fname #"
+        for count in {0..$(( ${#fname} + 9 ))};do
+            echo -n "#"
+        done
+        echo ""
+        cat $fname
+    done
+}
+zle -N multicat
