763edb4a3c7b990bb8685a4020451240462360fd
Oliver Breitwieser <oliver@breitwieser.eu>
2021-09-02 22:33:21 +0200

    Add functions to change defualt browser

---
+++ functions.d/browser.zsh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+browser_set_firefox() {
+    xdg-settings set default-web-browser firefox.desktop
+}
+
+browser_set_chrome() {
+    xdg-settings set default-web-browser google-chrome.desktop
+}
