8b708057a72e81994d149efe315a8a0f6bfa34cd
Oliver Breitwieser <oliver@breitwieser.eu>
2021-09-07 22:08:01 +0200

    Add log messages for browser switch

--- functions.d/browser.zsh
+++ functions.d/browser.zsh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 browser_set_firefox() {
+    echo "Setting default browser to firefox…">&2
     xdg-settings set default-web-browser firefox.desktop
 }
 
 browser_set_chrome() {
+    echo "Setting default browser to chrome…">&2
     xdg-settings set default-web-browser google-chrome.desktop
 }
