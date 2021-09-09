885906844f63e90e293b37930f2fe8ac40abcdcc
Oliver Breitwieser <oliver@breitwieser.eu>
2021-09-09 08:17:18 +0200

    Rename startx-wifi -> startx-with-net

--- functions.d/x11.zsh
+++ functions.d/x11.zsh
@@ -2,7 +2,9 @@ get-modeline() {
     cvt $1 $2 $3 | grep Modeline | sed -e "s/Modeline\s//"
 }
 
-startx-wifi() {
+startx-with-net() {
     # start the xserver with a given wifi profile
     sudo netctl start "${1}" &; startx
 }
+
+alias startx-wifi=startx-with-net
