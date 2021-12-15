5134e78a54c27b180ada96a929fd3bf127d65ad1
Oliver Breitwieser <oliver.breitwieser@gmail.com>
2021-12-15 11:57:19 +0100

    Add print_mail

--- functions.d/print.zsh
+++ functions.d/print.zsh
@@ -12,3 +12,20 @@ printp() {
 printc() {
     for a in "${@:2}"; do echo $1/$a; done
 }
+
+print_mail() {
+    local f_ps
+    local f_pdf
+    local mail_to_print
+    if (( $# == 0 )); then
+        echo "Need to specify mail!" >&2
+        return 1
+    fi
+    mail_to_print="${1}"
+    f_ps=$(mktemp -p "$HOME"/.cache/mutt -t print_mail_XXXXXXXX)
+    f_pdf=$(mktemp -p "$HOME"/.cache/mutt -t print_mail_XXXXXXXX.pdf)
+    muttprint -P A4 -p TO_FILE:"${f_ps}" < "${mail_to_print}"
+    ps2pdf ${f_ps} ${f_pdf}
+    rdocv "${f_pdf}"
+    rm "${f_ps}" "${f_pdf}"
+}
