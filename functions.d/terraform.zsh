54624d17c1589d5b087f3877013c9ca27bce0cb2
Oliver Breitwieser <oliver@breitwieser.eu>
2022-05-17 11:59:59 +0200

    Rename tf-planned -> tf-changes

--- functions.d/terraform.zsh
+++ functions.d/terraform.zsh
@@ -1,4 +1,9 @@
 
+# Get planned changes
+tf-changes() {
+    jq -r 'select( .type == "planned_change" ) | .change.resource.addr' <"$(tf-json)"
+}
+
 # Get temporary json file for the current terraform deployment
 tf-json() {
     pwd | awk -v FS=/ '{printf("/tmp/tf-%s-%s.json", $(NF-1), $NF)}'
@@ -7,9 +12,4 @@ tf-json() {
 # Plan terraform
 tf-plan() {
     terraform plan -json | tee "$(tf-json)" | jq -r '."@message"'
-}
-
-# Get planned changes
-tf-planned() {
-    jq -r 'select( .type == "planned_change" ) | .change.resource.addr' <"$(tf-json)"
 }
