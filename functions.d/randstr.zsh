eea460cd370406dfc00ff67a5eef288f3e8d6691
Oliver Breitwieser <oliver@breitwieser.eu>
2022-06-09 13:22:53 +0200

    Reimplement randstr

--- functions.d/randstr.zsh
+++ functions.d/randstr.zsh
@@ -1,9 +1,5 @@
 # generate a random string of length $1
 randstr() {
-    if [[ $# > 0 ]]; then
-        strlen="$1"
-    else
-        strlen="8"
-    fi
-    cat /dev/urandom 2>/dev/null | tr -cd "[:alnum:]" 2>/dev/null | head -c ${strlen}
+    strlen="${1:-8}"
+    tr -cd "[:alnum:]" </dev/urandom 2>/dev/null | head -c "${strlen}"
 }
