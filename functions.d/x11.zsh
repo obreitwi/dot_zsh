ff676d22d024fde48151e63b5ee006ab9b15e5a2
Oliver Breitwieser <oliver@breitwieser.eu>
2022-06-13 08:35:10 +0200

    Add autocompletion for startx-wifi

--- functions.d/x11.zsh
+++ functions.d/x11.zsh
@@ -3,7 +3,6 @@ get-modeline() {
 }
 
 startx-with-net() {
-
 if (( # < 1 )); then
     echo "Usage: startx-with-net <profile>" >&2
     return 1
@@ -19,4 +18,12 @@ fi
 sudo netctl start "${1}" &; startx
 }
 
+_startx-with-net() {
+local -a wifis
+wifis=("${(@f)$(find /etc/netctl -type f | xargs -L 1 basename)}")
+_values 'Wifi to connect on startup' "${wifis[@]}"
+}
+compdef _startx-with-net startx-with-net
+
 alias startx-wifi=startx-with-net
+compdef startx-wifi=startx-with-net
