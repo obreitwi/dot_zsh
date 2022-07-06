d410b941affe39e1feccf7f79d7370de758878f2
Oliver Breitwieser <oliver@breitwieser.eu>
2022-07-06 08:43:09 +0200

    Add both versions of proxies

--- functions.d/proxy.zsh
+++ functions.d/proxy.zsh
@@ -3,6 +3,10 @@ setkipproxy() {
     export http_proxy=http://proxy2.kip.uni-heidelberg.de:8080
     export ftp_proxy=http://proxy2.kip.uni-heidelberg.de:21
     export https_proxy=http://proxy2.kip.uni-heidelberg.de:8080
+    export NO_PROXY="$no_proxy"
+    export HTTP_PROXY="$http_proxy"
+    export FTP_PROXY="$ftp_proxy"
+    export HTTPS_PROXY="$http_proxy"
 }
 
 setnoproxy() {
@@ -10,4 +14,8 @@ setnoproxy() {
     unset http_proxy
     unset ftp_proxy
     unset https_proxy
+    unset NO_PROXY
+    unset http_proxy
+    unset FTP_PROXY
+    unset HTTPS_PROXY
 }
