3c4ba4bf6242b492a1e5fc667771e0b2c94db8a0
Oliver Breitwieser <oliver@breitwieser.eu>
2022-07-27 14:19:22 +0200

    Update bitbucket

--- functions.d/bitbucket.zsh
+++ functions.d/bitbucket.zsh
@@ -15,4 +15,11 @@ bb-current-url() {
     echo "https://$(bb-current-domain)/rest/api/1.0/projects/$(bb-current-project)/repos/$(bb-current-repo)"
 }
 
+bb-get() { # <endpoint>
+    curl -H "authorization: Bearer $(cat ~/.config/bitbucket/cli.token)" "$(bb-current-url)/$1"
+}
+
+bb-post() { # <endpoint>
+    curl --json @- -H "authorization: Bearer $(cat ~/.config/bitbucket/cli.token)" "$(bb-current-url)/$1"
+}
 
