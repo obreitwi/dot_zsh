cae8166b6432936b2d192f42e3d791e2165e726c
Oliver Breitwieser <oliver@breitwieser.eu>
2022-08-31 14:47:32 +0200

    Handle todos via quickfix list

--- functions.d/vimwiki.zsh
+++ functions.d/vimwiki.zsh
@@ -7,12 +7,15 @@ vimwiki_set_private() {
     ln -Tsfv ~/Nextcloud/vimwiki ~/.vimwiki
 }
 
+_todos_ex_grep="^\s*\\* \[[ .oO]\]"
+_todos_ex_sed="s/^\([^:]*\):\(\s*\)\\* \[[ .oO]\]/\1\2/"
+_todos_diary_path="$HOME/.vimwiki/diary"
+
 # Get all recent todos
 # Options:
 #   -d --days Number of days to check (default 7)
-get_todos() {
-    local diary_path="$HOME/.vimwiki/diary"
-    zparseopts -D -E -A args d: -days:
+get-todos() {
+    zparseopts -D -E -A args d: -days: -all
     local days="${args[-d]}"
     if [ -z "$days" ]; then
         days="${args[--days]}"
@@ -26,7 +29,7 @@ get_todos() {
 
     cutoff_date="$(($(date +%s) - 3600 * 24 * days))"
 
-    for entry_file in "${(@f)$(find "${diary_path}" -mindepth 1 -maxdepth 1 -type f | sort)}"; do
+    for entry_file in "${(@f)$(find "${_todos_diary_path}" -mindepth 1 -maxdepth 1 -type f | sort)}"; do
         entry_date=$(basename "${entry_file%%.md}")
 
         if (( $(date -d "$entry_date" +%s) < cutoff_date )); then
@@ -36,17 +39,19 @@ get_todos() {
         fi
     done
 
-    local grepex="^\s*\\* \[[ .oO]\]"
-    local sedex="s/^\([^:]*\):\(\s*\)\\* \[[ .oO]\]/\1\2/"
-    grep "${grepex}" "${(O)files_new[@]}" | sed -e "${sedex}"
+    # local grepex="^\s*\\* \[[ .oO]\]"
+    # local sedex="s/^\([^:]*\):\(\s*\)\\* \[[ .oO]\]/\1\2/"
+    grep "${_todos_ex_grep}" "${(O)files_new[@]}" | sed -e "${_todos_ex_sed}"
     echo -n "\n# Older:\n\n"
-    grep "${grepex}" "${(O)files_old[@]}" | sed -e "${sedex}"
+    grep "${_todos_ex_grep}" "${(O)files_old[@]}" | sed -e "${_todos_ex_sed}"
+}
+
+todos-errorfile() {
+    grep -rn "${_todos_ex_grep}" "${(@f)$(find "${_todos_diary_path}" -mindepth 1 -maxdepth 1 -type f | sort -r)}" \
+        | sed -e "s/\\* \[ \] //g"
 }
 
 # Open todos in vim
 todos() {
-    tmpfile=$(mktemp)
-    get_todos "${@}" >"${tmpfile}"
-    nvim "${tmpfile}"
-    rm -f "${tmpfile}"
+    nvim -q <(todos-errorfile) +copen
 }
