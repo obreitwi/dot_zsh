91578753c84caec48e0b792e64d364e414373e24
Oliver Breitwieser <oliver@breitwieser.eu>
2022-10-21 11:28:30 +0200

    Auto-exclude log-spam in datadog log queries

--- functions.d/datadog.zsh
+++ functions.d/datadog.zsh
@@ -1,11 +1,12 @@
+dd-log-clean() {
+    echo -n '-"Datadog Tracer" -"warning envoy config" -image_name:*istio\/*'
+}
+
 dd-log() { # <query>...
-    xdg-open "https://app.datadoghq.eu/logs?query=$(echo -n "$*" | urlencode)"
+    xdg-open "https://app.datadoghq.eu/logs?query=$(echo -n "$* $(dd-log-clean)" | urlencode)"
 }
 
 dd-trace() { # <query>...
     xdg-open "https://app.datadoghq.eu/apm/traces?query=$(echo -n "$*" | urlencode)"
 }
 
-dd-log-clean() {
-    echo -n '-"Datadog Tracer" -"warning envoy config" -image_name:*istio\/*'
-}
