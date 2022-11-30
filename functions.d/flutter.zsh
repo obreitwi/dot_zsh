fb001669880026fa63b95b4405c96c4055bc4a9a
Oliver Breitwieser <oliver@breitwieser.eu>
2022-12-01 00:15:08 +0100

    Add flutter-test

--- functions.d/flutter.zsh
+++ functions.d/flutter.zsh
@@ -2,3 +2,7 @@
 flutter-pub-get() {
     (cd "$1"; flutter pub get; git checkout pubspec.lock)
 }
+
+flutter-test() {
+    (cd "$1"; make test; git checkout pubspec.lock) | tee make_test_${1/\//_}.log
+}
