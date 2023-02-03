ebd2f63da94904e14e36af8fdc7a1702ea4904bf
Oliver Breitwieser <oliver.breitwieser@freiheit.com>
2023-02-03 17:21:43 +0100

    Fix typo

--- functions.d/wiki.zsh
+++ functions.d/wiki.zsh
@@ -10,7 +10,7 @@ wiki_set_private() {
 # neorg
 _todos_ex_grep='^\s*\(\*\|-\) \(( )\|\[[ .oO]\]\)'
 _todos_ex_sed='s/^\([^:]*\):\(\s*\)\(\*\|-\) \(([ x])\|\[[ .oO]\]\)/\1\2/'
-_todos_diary_path="$HOME/mwiki/neorg/journal"
+_todos_diary_path="$HOME/wiki/neorg/journal"
 
 # Get all recent todos
 # Options:
