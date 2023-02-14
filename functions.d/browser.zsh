c82f3101c6c34a2747871b116c85e4bc7d6d336c
Oliver Breitwieser <oliver.breitwieser@freiheit.com>
2023-02-14 23:07:11 +0100

    Add option to switch to custom auto profile

--- functions.d/browser.zsh
+++ functions.d/browser.zsh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-browser_set_firefox() {
-    echo "Setting default browser to firefox…">&2
-    xdg-settings set default-web-browser firefox.desktop
+browser_set_chrome_auto_profile() {
+    echo "Setting default browser to chrome-auto-profile…">&2
+    xdg-settings set default-web-browser chrome-auto-profile.desktop
 }
 
 browser_set_chrome() {
     echo "Setting default browser to chrome…">&2
     xdg-settings set default-web-browser google-chrome.desktop
+}
+
+browser_set_firefox() {
+    echo "Setting default browser to firefox…">&2
+    xdg-settings set default-web-browser firefox.desktop
 }
