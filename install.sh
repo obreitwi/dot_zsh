2376080bfbc299f9bca1afe81ac7a5ed92b1a758
Oliver Breitwieser <oliver.breitwieser@freiheit.com>
2023-04-25 16:07:49 +0200

    Add starship config

--- install.sh
+++ install.sh
@@ -9,7 +9,10 @@ symlink () {
 ln -s -f -v $*
 }
 
+mkdir -p "${HOME}/.config" || true
+
 symlink $SRCFLD $HOME/.zsh
 symlink $SRCFLD/zshrc $HOME/.zshrc
 symlink $SRCFLD/zprofile $HOME/.zprofile
+symlink $SRCFLD/starship/starship.toml $HOME/.config
 
