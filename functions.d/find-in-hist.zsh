ee454ec22c43488e818e5437f115eebbdc2976f2
Oliver Breitwieser <oliver@breitwieser.eu>
2023-06-20 14:54:03 +0200

    Rename history plugins

--- functions.d/find-in-hist.zsh
+++ functions.d/find-in-hist.zsh
@@ -1,5 +1,19 @@
 find_in_hist() {
-    zgrep -i "$1" ~/.logs/*.gz
-    grep -ri "$1" ~/.logs
+    if command -v ugrep &>/dev/null; then
+        ugrep -zG "$@" ~/.logs
+    else
+        zgrep "$@" ~/.logs/*.gz
+        grep -r "$@" ~/.logs
+    fi
 }
-alias fih=find_in_hist
+alias hif=find_in_hist
+
+copy_from_history() {
+    local -a args
+    args=( "$@" )
+    if (( ${#args} == 0 )); then
+        args+=""
+    fi
+    ugrep -zG -I --no-filename "${args[@]}" ~/.logs | sed -e "s#^$USER@$HOST:.*\[[^]]*\]\s##g" | sort | uniq | fzf | xclip -i -selection clipboard
+}
+alias hic=copy_from_history
