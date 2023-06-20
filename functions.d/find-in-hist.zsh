59cb1353a4626150f4fc0541250a67cebfc794bc
Oliver Breitwieser <oliver@breitwieser.eu>
2023-06-20 16:20:52 +0200

    Add a way to search all history files

--- functions.d/find-in-hist.zsh
+++ functions.d/find-in-hist.zsh
@@ -8,12 +8,34 @@ find_in_hist() {
 }
 alias hif=find_in_hist
 
-copy_from_history() {
+get_from_history() {
     local -a args
     args=( "$@" )
     if (( ${#args} == 0 )); then
         args+=""
     fi
-    ugrep -zG -I --no-filename "${args[@]}" ~/.logs | sed -e "s#^$USER@$HOST:.*\[[^]]*\]\s##g" | sort | uniq | fzf | xclip -i -selection clipboard
+    get_history_lines_p0 \
+        | ugrep -G -a "${args[@]}" \
+        | sed -e "s#^$USER@$HOST:.*\[[^]]*\]\s##g" | sort | uniq | fzf | tr '\0' '\n'
+}
+
+# newlines within entries are 0-encoded
+get_history_lines_p0() {
+    {
+        find ~/.logs -type f -name "*.log" | xargs -r cat
+        find ~/.logs -type f -name "*.log.gz" | xargs -r zcat
+    } | tr '\n' '\0' | sed -e "s#\x0$USER@$HOST#\n$USER@$HOST#g"
+}
+
+copy_from_history() {
+    get_from_history "$@" | xclip -i -selection clipboard
 }
 alias hic=copy_from_history
+
+insert-from-history() {
+    local -a args
+    # clear-line
+    printf '\x1B[1K\r'
+    args=( $(gum input --placeholder "Search in history" --prompt "❯ ") )
+    BUFFER=$(get_from_history "${args[@]}")
+}
