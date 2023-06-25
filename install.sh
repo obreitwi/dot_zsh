3a06847aafd6a7c9d80c0cd95d5d6a184279dac3
Oliver Breitwieser <oliver@breitwieser.eu>
2023-06-25 13:36:28 +0200

    Update install script

--- install.sh
+++ install.sh
@@ -3,17 +3,18 @@
 # Quick install script to setup all symlinks
 
 # SRCFLD=$(dirname $(realpath $0))
-SRCFLD=$(dirname $(readlink -m "$0"))
+SRCFLD=$(dirname "$(readlink -m "$0")")
 
 symlink () {
-ln -s -f -v $*
+ln -s -f -v "$@"
 }
 
 mkdir -p "${HOME}/.config" || true
 
-symlink $SRCFLD $HOME/.zsh
-symlink $SRCFLD/zshrc $HOME/.zshrc
-symlink $SRCFLD/zprofile $HOME/.zprofile
-symlink $SRCFLD/starship/starship.toml $HOME/.config
-symlink $SRCFLD/p10k/p10k.zsh $HOME/.p10k.zsh
+! [ -L "$HOME/.zsh" ] && symlink "$SRCFLD" "$HOME/.zsh"
+symlink "$SRCFLD/zshrc" "$HOME/.zshrc"
+symlink "$SRCFLD/zprofile" "$HOME/.zprofile"
+symlink "$SRCFLD/starship/starship.toml" "$HOME/.config"
+symlink "$SRCFLD/p10k/p10k.zsh" "$HOME/.p10k.zsh"
+symlink "$SRCFLD/plugins/forgit/bin/git-forgit" "$HOME/.local/bin"
 
