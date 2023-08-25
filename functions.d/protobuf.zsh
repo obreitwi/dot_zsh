c82e7226fa406ee67ea461978b609a8f46ecf033
Oliver Breitwieser <oliver@breitwieser.eu>
2023-08-25 13:23:20 +0200

    Add helper to find proto messages

---
+++ functions.d/protobuf.zsh
@@ -0,0 +1,10 @@
+#!/usr/bin/env zsh
+
+# find proto message in current git repo
+proto-msg() {
+   find $(git-root) -type f -name "*.proto" -print0 | xargs -0 grep -n '^\s*message' | sed -e 's:\s*{}\?\s*$::' | fzf --with-nth 2 | sed 's/:[^:]*$//g'
+}
+
+nvim-proto() {
+   nvim "$(proto-msg)"
+}
