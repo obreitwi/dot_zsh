538b190713d97e4721ed374d237bdde23ecf8988
Oliver Breitwieser <oliver@breitwieser.eu>
2023-08-29 11:00:40 +0200

    Handle empty proto messages

--- functions.d/protobuf.zsh
+++ functions.d/protobuf.zsh
@@ -11,11 +11,19 @@ proto-msg() {
 proto-bat() {
    local info
    local file
+   local line_no
+   local line
    local msg
    info=$(proto-msg-raw)
    msg=$(awk '{ print $NF }' <<<"$info")
    file=$(awk -F : '{ print $1 }' <<<"$info")
-   sed -n "/^message $msg\s*{/,/}/p" "$file" | bat -l proto
+   line_no=$(awk -F : '{ print $2 }' <<<"$info")
+   line=$(tail -n "+$line_no" < "$file" | head -n 1)
+   if grep -qF '{}' <<<"$line"; then
+      bat -l proto <<<"$line"
+   else
+      sed -n "/^message $msg\s*{/,/}/p" "$file" | bat -l proto
+   fi
 }
 
 proto-nvim() {
