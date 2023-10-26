0322107c25f0565a8ee6552d1453835423cb5753
Oliver Breitwieser <oliver@breitwieser.eu>
2023-10-26 16:59:52 +0200

    to_gif: Add OUTPUT variable

--- functions.d/conversion.zsh
+++ functions.d/conversion.zsh
@@ -75,7 +75,7 @@ to_mp3() {
 
 to_gif() {
     for input in "${@}"; do
-        ffmpeg -i ${input} -vf "fps=${FPS:-10},scale=-1:-1:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse" -loop 0 "${input:t:r}.gif"
+        ffmpeg -i ${input} -vf "fps=${FPS:-10},scale=-1:-1:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse" -loop 0 "${OUTPUT:-${input:t:r}.gif}"
     done
 }
 
