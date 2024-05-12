fc1c6a8caac2af42fb3fc3017c964d3e5a3d1b33
Oliver Breitwieser <oliver@breitwieser.eu>
2024-05-12 21:27:54 +0200

    feat(nsh): allow for regular arguments to be passed to 'nix shell'

--- functions.d/nix.zsh
+++ functions.d/nix.zsh
@@ -4,6 +4,21 @@ nsd() {
    nix derivation show "$@" | jq
 }
 
-nsh() { # <packages>
-   nix-shell --run zsh -p "let pkgs = (import <nixpkgs-unstable> {}); in with pkgs; [ $* ]"
+nsh() { # <packages> -- <other args to `nix shell`>
+   local -a pkgs
+   local end_of_packages=0
+   while (( $# > 0  && end_of_packages == 0 )); do
+
+      case $1 in
+         --)
+            end_of_packages=1
+         ;;
+         *)
+            pkgs+=("$1")
+         ;;
+      esac
+      shift 1
+   done
+
+   nix shell "${pkgs[@]/#/nixpkgs#}" "$@"
 }
