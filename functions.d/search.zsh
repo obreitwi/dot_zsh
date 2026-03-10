9acb238b4d802b70a8dde21f4701d445737d5f15
Oliver Breitwieser <oliver@breitwieser.eu>
2026-03-10 20:46:19 +0100

    fix: github search url not working

--- functions.d/search.zsh
+++ functions.d/search.zsh
@@ -1,5 +1,9 @@
 
+search_nixpkgs_url() {
+    echo "https://github.com/search?q=$(echo repo:NixOS/nixpkgs "$@" | tr -d '\n' | urlencode)"
+}
+
 search_nixpkgs() { 
-    xdg-open "https://github.com/search?q=$(urlencode <<< repo:NixOS/nixpkgs "$@")"
+    xdg-open "$(search_nixpkgs_url "$@")"
 }
 alias sn=search_nixpkgs
